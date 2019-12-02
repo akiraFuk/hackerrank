321227acb21055705d9c5c8b65acf2e4ffb05d7c
Felipe Akira Fukue <akirarpy@gmail.com>
2019-12-01 21:32:14 -0300

    Create d1-pi.js

---
+++ 10djs/d1-pi.js
@@ -0,0 +1,10 @@
+    // Print the area of the circle:
+    const r = readLine();
+    const PI = 3.14159265358979323846;
+
+    let area = PI * r ** 2;
+    console.log(area);
+    
+    // Print the perimeter of the circle:
+    let perimeter = 2 * PI * r;
+    console.log(perimeter);
